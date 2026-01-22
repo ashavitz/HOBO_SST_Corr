db15057cba60ff9321975737834f27d220cf7cdf
Aaron Shavitz <aaron.shavitz@gmail.com>
2026-01-22 18:56:07 -0500

    Initial commit for light data summary

---
+++ HOBO_Deployment_Stats.R
@@ -0,0 +1,8 @@
+# Import HOBO data
+
+
+# Determine deployment date(s) for each site
+
+# Filter only days 2-14 post-deployment, for each site
+
+# Calculate summary statistics (mean, median, min, max) for each deployment
